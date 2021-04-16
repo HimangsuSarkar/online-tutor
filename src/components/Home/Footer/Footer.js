127414edde0c4634ace1dad67f135db4ded63745
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-04-16 22:48:28 +0800

    design footer

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -4,43 +4,44 @@ import React from 'react';
 import FooterCol from '../FooterCol/FooterCol';
 import './Footer.css'
 const Footer = () => {
-    const noNamed = [
-        { name: "Emergency Dental Care", link: "/emergency" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Treatment of Personal Diseases", link: "/personal-treatment" },
-        { name: "Tooth Extraction", link: "/tooth-extract" },
-        { name: "Check Up", link: "/checkup" },
+    const know_us = [
+        { name: "About Us", link: "/about" },
+        { name: "Help Center", link: "/help" },
+        { name: "Blog", link: "/blog" },
+        { name: "Accessibility", link: "/accessibility" },
+        { name: "Careers", link: "/careers" },
+        { name: "Honor Code", link: "/honorCode" },
     ]
     const ourAddress = [
-        { name: "New York - 101010 Hudson", link: "//google.com/map" },
-        { name: "Yards", link: "//google.com/map" },
+        { name: "Jiangsu- 12783 Wujin", link: "//google.com/map" },
+        { name: "China", link: "//google.com/map" },
 
     ]
-    const oralHealth = [
-        { name: "Emergency Dental Care", link: "/emergency" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Treatment of Personal Diseases", link: "/personal-treatment" },
-        { name: "Tooth Extraction", link: "/tooth-extract" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Check Up", link: "/checkup" }
+    const Learn_with_us = [
+        { name: "Become a Tutor", link: "/becomeTutor" },
+        { name: "Science", link: "/science" },
+        { name: "Computer Science", link: "/computerScience" },
+        { name: "Test Prep Course", link: "/testPrep" },
+        { name: "Engineering", link: "/engineering" },
+        { name: "Profession", link: "/profession" },
+        { name: "Foreign Language", link: "/foreignLanguage" }
     ]
     const services = [
-        { name: "Emergency Dental Care", link: "/emergency" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Treatment of Personal Diseases", link: "/personal-treatment" },
-        { name: "Tooth Extraction", link: "/tooth-extract" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Check Up", link: "/checkup" },
-        { name: "Check Up", link: "/checkup" }
+        { name: "Writing Tutors", link: "/writingTutor" },
+        { name: "English Tutors", link: "/englishTutor" },
+        { name: "Computer Science", link: "/computerScience" },
+        { name: "Accounting Tutors", link: "/accountingTutor" },
+        { name: "Finance Tutors", link: "/financeTutor" },
+        { name: "Basic Math Tutors", link: "/mathTutor" },
+        { name: "Chemistry Tutors", link: "/chemistryTutor" }
     ]
     return (
         <footer className="footer-area clear-both">
             <div className="container pt-5">
                 <div className="row py-5">
-                    <FooterCol key={1} menuTitle={"."} menuItems={noNamed} />
+                    <FooterCol key={1} menuTitle={"Get To Know Us"} menuItems={know_us} />
                     <FooterCol key={2} menuTitle="Services" menuItems={services} />
-                    <FooterCol key={3} menuTitle="Oral Health" menuItems={oralHealth} />
+                    <FooterCol key={3} menuTitle="Learn With Us" menuItems={Learn_with_us} />
                     <FooterCol key={4} menuTitle="Our Address" menuItems={ourAddress}>
                         <ul className="social-media list-inline">
                             <li className="list-inline-item"><a href="//facebook.com"><FontAwesomeIcon className="icon active-icon" icon={faFacebookF} /></a></li>
@@ -49,7 +50,7 @@ const Footer = () => {
                         </ul>
                         <div className="mt-5">
                             <h6>Call now</h6>
-                            <button className="btn btn-primary">+2025550295</button>
+                            <button className="btn btn-primary">+8615695203280</button>
                         </div>
                     </FooterCol>
                 </div>
